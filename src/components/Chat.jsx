3cda3ad20fdc94c2e940af0b4e633edae7f7c62e
Greta <50132963+GretaVait@users.noreply.github.com>
2020-12-09 14:35:33 +0200

    chat list styling

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,22 +4,20 @@ import { Media, Image } from 'react-bootstrap';
 const Chat = (props) => {
 
   return (
-    <div>
-      <Media as="li">
-        <Image
-          width={64}
-          height={64}
-          className="mr-3"
-          src={props.contact.image}
-          alt=""
-          roundedCircle
-        />
-        <Media.Body>
-          <h5>{props.contact.name}</h5>
-          <p>{props.lastMessage}</p>
-        </Media.Body>
-      </Media>
-    </div>
+    <Media as="li" className="contact">
+      <Image
+        width={64}
+        height={64}
+        className="mr-3"
+        src={props.contact.image}
+        alt=""
+        roundedCircle
+      />
+      <Media.Body className="contact__content">
+        <h5>{props.contact.name}</h5>
+        <p className="contact__content__faded">{props.lastMessage}</p>
+      </Media.Body>
+    </Media>
   );
 }
 
