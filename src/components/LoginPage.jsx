7afdb9d3127e4f3002542a5e6a7e13d1fa2ad74d
Greta <50132963+GretaVait@users.noreply.github.com>
2020-12-09 16:17:35 +0200

    Login component styling

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -13,20 +13,22 @@ const LoginPage = () => {
   }
   return (
     <Container className="flex-column align-items-center justify-content-center d-flex" style={{ height: '100vh', width: '100vw' }}>
-      <h1>Login</h1>
-      <Form onSubmit={startLogin}>
-        <Form.Group>
-          <Form.Label>Enter Your Email</Form.Label>
-          <Form.Control type="email" id="email" required />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Enter Your Password</Form.Label>
-          <Form.Control type="password" id="password" required />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+      <div className="login">
+        <h1 className="login__title">Login</h1>
+        <Form onSubmit={startLogin}>
+          <Form.Group>
+            <Form.Label>Enter Your Email</Form.Label>
+            <Form.Control type="email" id="email" required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Enter Your Password</Form.Label>
+            <Form.Control type="password" id="password" required />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </div>
     </Container>
   );
 }
