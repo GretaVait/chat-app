062ece8b50185d188fa1c7816863b947adb0688e
Greta <50132963+GretaVait@users.noreply.github.com>
2020-12-09 15:47:49 +0200

    edit profile styling

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useRef } from 'react';
 
+import { Form, Image, Button } from 'react-bootstrap';
+
+import { FaArrowLeft, FaPen } from 'react-icons/fa';
+
 const EditProfile = (props) => {
   const [ image, setImage ] = useState(props.user.image);
   const hiddenFileInput = useRef(null);
@@ -23,16 +27,31 @@ const EditProfile = (props) => {
   }
 
   return (
-    <div>
-      <button onClick={props.toggleEditProfile}>Hide Edit Profile</button>
-      <form onSubmit={updateHandler}>
-        <input style={{display: 'none'}} type="file" name="avatar" ref={hiddenFileInput} onChange={changeImageHandler} />
-        <div onClick={pickImageHandler}>
-          <img src={image} alt="" width="50px" />
-        </div>
-        <input type="text" defaultValue={props.user.name} name="username" />
-        <button>Update</button>
-      </form>
+    <div className="edit-profile">
+      <div className="edit-profile__header">
+        <button onClick={props.toggleEditProfile} className="btn btn--back"><FaArrowLeft /></button>
+        <h5>Profile</h5>
+      </div>
+      <Form className="edit-profile__form">
+        <Form.Group>
+          <Form.Control style={{display: 'none'}} type="file" name="avatar" ref={hiddenFileInput} onChange={changeImageHandler} />
+          <div onClick={pickImageHandler} className="edit-profile__form__image">
+            <Image
+              width={200}
+              height={200}
+              className="mr-3"
+              src={image}
+              alt=""
+              roundedCircle
+            />
+          </div>
+          <Form.Control type="text" defaultValue={props.user.name} name="username" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Update <FaPen />
+        </Button>
+      </Form>
     </div>
   );
 }
